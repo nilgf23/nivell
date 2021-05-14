074881f08a5cab6a1493905eb3dba67f664760f6
nilgf23 <82875774+nilgf23@users.noreply.github.com>
2021-05-14 14:09:56 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,26 +1,16 @@
-let x = 2
-let y = 2
-led.plot(x, y)
+let sprite = game.createSprite(2, 2)
 basic.forever(function () {
-    basic.pause(1000)
-    if (input.acceleration(Dimension.X) >= 341) {
-        led.unplot(x, y)
-        x += 1
-        led.plot(x, y)
+    basic.pause(500)
+    if (input.acceleration(Dimension.X) < -300) {
+        sprite.change(LedSpriteProperty.X, -1)
     }
-    if (input.acceleration(Dimension.Y) >= 341) {
-        led.unplot(x, y)
-        y += 1
-        led.plot(x, y)
+    if (input.acceleration(Dimension.X) > 300) {
+        sprite.change(LedSpriteProperty.X, 1)
     }
-    if (input.acceleration(Dimension.Y) >= -341) {
-        led.unplot(x, y)
-        y += -1
-        led.plot(x, y)
+    if (input.acceleration(Dimension.Y) < -300) {
+        sprite.change(LedSpriteProperty.Y, -1)
     }
-    if (input.acceleration(Dimension.X) >= -341) {
-        led.unplot(x, y)
-        y += -1
-        led.plot(x, y)
+    if (input.acceleration(Dimension.Y) > 300) {
+        sprite.change(LedSpriteProperty.Y, 1)
     }
 })
